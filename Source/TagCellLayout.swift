70042d8d01630d52ea3b66457264a1b8afe4b443
Admin <admin@Rash-Macbook.local>
2017-02-02 12:55:16 +0330

    modified white space 2

--- Source/TagCellLayout.swift
+++ Source/TagCellLayout.swift
@@ -39,7 +39,8 @@ public class TagCellLayout: UICollectionViewLayout {
 		
 		init(layoutAttribute: UICollectionViewLayoutAttributes) {
 			self.layoutAttribute = layoutAttribute
-			self.whiteSpace = 10
+			self.whiteSpace = 12
+            
 		}
 	}
 	
